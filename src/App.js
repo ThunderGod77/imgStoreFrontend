ac1a50c129f502e0c0331dfae2337e1b67a7196e
kakkashi_76 <kshitijgang76@gmail.com>
2020-11-26 15:55:45 +0530

    starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+
 import './App.css';
 
 function App() {
+  let handleSubmit = ()=>{
+    console.log("kik");
+  }
+  const [title,updateTitle] = useState("");
+  const [content,updateContent] = useState("");
+  const [image,updateImage] = useState(null);
+  const changeTitle  = (e)=>{
+    updateTitle(e.target.value);
+  }
+  const changeContent = (e)=>{
+    updateContent(e.target.value);
+  }
+  const changeImage = (e)=>{
+    updateImage(e.target.files[0]);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <form /**onSubmit={this.handleSubmit}**/>
+          <p>
+            <input type="text" placeholder='Title' id='title' value={title} onChange={changeTitle} required/>
+          </p>
+          <p>
+            <input type="text" placeholder='Content' id='content' value={content} onChange={changeContent} required/>
+
+          </p>
+          <p>
+            <input type="file"
+                   id="image"
+                   accept="image/png, image/jpeg"  /**onChange={this.handleImageChange}**/ required/>
+          </p>
+          <input type="submit"/>
+        </form>
+      </div>
     </div>
   );
 }
