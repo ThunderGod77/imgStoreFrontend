8065b307d4542ac305122d9d563f0e95bf8779a1
kakkashi_76 <kshitijgang76@gmail.com>
2020-11-30 21:35:11 +0530

    navbarDone

--- src/App.js
+++ src/App.js
@@ -1,60 +1,63 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import Axios from 'axios';
 import FormData from 'form-data';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import NavBar from './Components/NavBar.js'
+
 
 import './App.css';
 
 function App() {
 
-  const [title,updateTitle] = useState("");
-  const [content,updateContent] = useState("");
-  const [image,updateImage] = useState(null);
-  const changeTitle  = (e)=>{
+  const [title, updateTitle] = useState("");
+  const [content, updateContent] = useState("");
+  const [image, updateImage] = useState(null);
+  const changeTitle = (e) => {
     updateTitle(e.target.value);
   }
-  const changeContent = (e)=>{
+  const changeContent = (e) => {
     updateContent(e.target.value);
   }
-  const changeImage = (e)=>{
+  const changeImage = (e) => {
     updateImage(e.target.files[0]);
   }
   const url = "http://localhost:3000/upload"
-  const imgSubmit = async(e)=>{
+  const imgSubmit = async (e) => {
     e.preventDefault();
-    var form = new FormData();
+    let form = new FormData();
     form.append("lol", image);
     form.append('foo', 'bar');
+    form.append('name', title);
+    form.append('description', content);
+
     const response = await Axios.post(url, form);
     console.log(response);
 
   }
 
+  return (<div className="App">
+    <BrowserRouter>
+      <NavBar/>
+      <Switch>
+        <Route path={"/upload"} exact="exact">
+          <form onSubmit={imgSubmit}>
+            <p>
+              <input type="text" placeholder='Title' id='title' value={title} onChange={changeTitle} required="required"/>
+            </p>
+            <p>
+              <input type="text" placeholder='Content' id='content' value={content} onChange={changeContent} required="required"/>
 
-  return (
+            </p>
+            <p>
+              <input type="file" id="image" accept="image/png, image/jpeg" onChange={changeImage} required="required"/>
+            </p>
+            <input type="submit"/>
+          </form>
+        </Route>
+      </Switch>
+    </BrowserRouter>
 
-      <div className="App">
-        <form onSubmit={imgSubmit}>
-          <p>
-            <input type="text" placeholder='Title' id='title' value={title} onChange={changeTitle} required/>
-          </p>
-          <p>
-            <input type="text" placeholder='Content' id='content' value={content} onChange={changeContent} required/>
-
-          </p>
-          <p>
-            <input type="file"
-                   id="image"
-                   accept="image/png, image/jpeg"  onChange={changeImage} required/>
-          </p>
-          <input type="submit"/>
-        </form>
-        {/*<div>*/}
-        {/*  <img src={"https://myspacelol.fra1.digitaloceanspaces.com/namewater.png"}/>*/}
-        {/*</div>*/}
-      </div>
-
-
-  );
+  </div>);
 }
 
 export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import NotListedLocationIcon from '@material-ui/icons/NotListedLocation';
+import MoreIcon from '@material-ui/icons/More';
+import SearchIcon from '@material-ui/icons/Search';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+
+function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (<div>
+    <Navbar color="dark" light="light" expand="md" style={{
+        color: "white"
+      }}>
+      <NavbarBrand href="/" style={{
+          color: "white"
+        }}><PhotoLibraryIcon/>7 Chan</NavbarBrand>
+      <NavbarToggler onClick={toggle}/>
+      <Collapse isOpen={isOpen} navbar="navbar">
+        <Nav className="mr-auto" navbar="navbar">
+          <NavItem>
+            <NavLink style={{
+                color: "white"
+              }}><BarChartIcon/>Top</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink style={{
+                color: "white"
+              }}><NotListedLocationIcon/>Random</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav="nav" inNavbar="inNavbar">
+            <DropdownToggle nav="nav" caret="caret" style={{
+                color: "white"
+              }}>
+              <MoreIcon/>{' Extra'}
+            </DropdownToggle>
+            <DropdownMenu right="right" style={{
+                backgroundColor: "#343A40"
+              }}>
+              <DropdownItem style={{
+                  color: "white",
+                  backgroundColor: "#343A40"
+                }}>
+                About
+              </DropdownItem>
+              <DropdownItem style={{
+                  color: "white",
+                  backgroundColor: "#343A40"
+                }}>
+                Contribute
+              </DropdownItem>
+
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+        <NavbarText style={{
+            color: "white"
+          }}>
+          <InputGroup>
+
+
+              <SearchIcon fontSize="large"/>
+            <Input placeholder={"Search"} size="sm"/>
+          </InputGroup>
+        </NavbarText>
+      </Collapse>
+    </Navbar>
+  </div>);
+}
+
+export default Navigation;
